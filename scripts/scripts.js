db054e6c66ff81ce82163dcc2e60665594cbf226
Carlos <iuryalencar@poli.ufrj.br>
2021-07-22 15:03:59 -0300

    Refactoring - Excluded loops and repetitive select functions

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,86 +1,118 @@
-let plateSelected,drinkSelected,dessertSelected = false;
+// Declaring last selected controllers || -1 means that no item was selected
+let lastDessertSelected = -1;
+let lastPlateSelected = -1;
+let lastDrinkSelected = -1;
 
 
 // Creating global objects
-function plate (plateName,plateCost) {
+function plate (plateSelected,plateName,plateCost) {
     this.plateName = plateName;
     this.plateCost = plateCost;
+    this.plateSelected = plateSelected;
 }
+
 let myPlate = new plate;
-function drink (drinkName,drinkCost) {
+
+function drink (drinkSelected,drinkName,drinkCost) {
     this.drinkName = drinkName;
     this.drinkCost = drinkCost;
+    this.drinkSelected = drinkSelected;
+
 }
+
 let myDrink = new drink;
-function dessert (dessertName,dessertCost) {
+
+function dessert (dessertSelected,dessertName,dessertCost) {
     this.dessertName = dessertName;
     this.dessertCost = dessertCost;
+    this.plateSelected = dessertSelected;
 }
+
 let myDessert = new dessert;
+// ----------------------------------------
 
-function selectPlate(num,plateName, plateCost) {
+
+
+function selectItem(itemId,itemName, itemCost) {
     const plates = document.getElementsByClassName("plates");
-
-
-    // Unchecking all plates
-    for (i = 0; i < plates.length; i++){
-        plates[i].classList.remove("choosen");
-        plates[i].children[4].style.display = "none";
-    }
-
-    // Checking one plate
-    plates[num].classList.add("choosen");
-    plates[num].children[4].style.display = "inherit";
-
-    plateSelected = true;
-    myPlate.plateName = plateName;
-    myPlate.plateCost = plateCost;
-
-    enableSendButton();
-}
-
-function selectDrink(num, drinkName,drinkCost) {
     const drinks = document.getElementsByClassName("drinks");
-    // Unchecking all drinks
-    for (i = 0; i < drinks.length; i++){
-        drinks[i].classList.remove("choosen");
-        drinks[i].children[4].style.display = "none";
-    }
-
-    // Checking one plate
-    drinks[num].classList.add("choosen");
-    drinks[num].children[4].style.display = "inherit"
-
-    drinkSelected = true;
-    myDrink.drinkName = drinkName;
-    myDrink.drinkCost = drinkCost;
-
-    enableSendButton();
-    
-}
-
-function selectDessert(num,dessertName,dessertCost) {
     const desserts = document.getElementsByClassName("desserts");
-    // Unchecking all desserts
-    for (i = 0; i < desserts.length; i++){
-        desserts[i].classList.remove("choosen");
-        desserts[i].children[4].style.display = "none"
-    }
 
-    // Checking one plate
-    desserts[num].classList.add("choosen");
-    desserts[num].children[4].style.display = "inherit"
- 
-    dessertSelected = true;
-    myDessert.dessertName = dessertName;
-    myDessert.dessertCost = dessertCost;
+    // If item selected is a plate
+    if (itemId[0] === "0") {
+
+        // unmark last plate selected || -1 means that no drink was selected
+        if (lastPlateSelected !== -1 ) {
+            plates[lastPlateSelected].classList.remove("choosen");
+            plates[lastPlateSelected].children[4].style.display = "none"
+        }
+
+        // mark new plate
+        plates[Number(itemId[1])].classList.add("choosen");
+        plates[Number(itemId[1])].children[4].style.display = "initial";
+
+        // update last plate selected
+        lastPlateSelected = Number(itemId[1]);
+
+        // Save plate name and cost
+        myPlate.plateName = itemName;
+        myPlate.plateCost = itemCost;
+        myPlate.plateSelected = true;
+
+    }
+    
+    // If item selected is a drink
+    if (itemId[0] === "1") {
+
+        // unmark last drink selected || -1 means that no drink was selected
+        if (lastDrinkSelected !== -1 ) {
+            drinks[lastDrinkSelected].classList.remove("choosen");
+            drinks[lastDrinkSelected].children[4].style.display = "none";
+        }
+
+        // mark new drink
+        drinks[Number(itemId[1])].classList.add("choosen");
+        drinks[Number(itemId[1])].children[4].style.display = "initial";
+
+        // update last drink selected
+        lastDrinkSelected = Number(itemId[1]);
+
+        // Save Drink name and cost
+        myDrink.drinkName = itemName;
+        myDrink.drinkCost = itemCost;
+        myDrink.drinkSelected = true;
+
+    }
+    
+    // If item selected is a dessert
+    if (itemId[0] === "2") {
+
+        // unmark last item selected || -1 means that no drink was selected
+        if (lastDessertSelected !== -1 ) {
+            desserts[lastDessertSelected].classList.remove("choosen");
+            desserts[lastDessertSelected].children[4].style.display = "none";
+        }   
+
+        // mark new drink
+        desserts[Number(itemId[1])].classList.add("choosen");
+        desserts[Number(itemId[1])].children[4].style.display = "initial";
+
+        // update last drink selected
+        lastDessertSelected = Number(itemId[1]);
+
+        // Save dessert name and cost
+        myDessert.dessertName = itemName;
+        myDessert.dessertCost = itemCost;
+        myDessert.dessertSelected = true;
+    }
+    
 
     enableSendButton();
 }
 
 function enableSendButton() {
 
-    if (plateSelected && drinkSelected && dessertSelected) {
+    if (myPlate.plateSelected && myDrink.drinkSelected && myDessert.dessertSelected) {
         const sendButton = document.getElementById("sendButton");
     
         sendButton.style.backgroundColor = "#32B72F";
@@ -94,38 +126,49 @@ function enableSendButton() {
 function openConfirmWindow() {
     const orderItens = document.getElementsByClassName("confirm-order-item");
 
+    // Writing plate infos on confirm window
     orderItens[0].children[0].innerHTML = myPlate.plateName;
     orderItens[0].children[1].innerHTML = myPlate.plateCost.toFixed(2);
 
-
+    // Writing drink infos on confirm window
     orderItens[1].children[0].innerHTML = myDrink.drinkName;
     orderItens[1].children[1].innerHTML = myDrink.drinkCost.toFixed(2);
 
+    // Writing dessert infos on confirm window
     orderItens[2].children[0].innerHTML = myDessert.dessertName;
     orderItens[2].children[1].innerHTML = myDessert.dessertCost.toFixed(2);
 
+    // Calculating and writing total
     const finalCost = myPlate.plateCost + myDrink.drinkCost + myDessert.dessertCost;
-
     orderItens[3].children[1].innerHTML = finalCost.toFixed(2);
 
+    // Show confirm window (changing display none to flex)
     const confirmWindow = document.getElementById("confirmWindow");
     confirmWindow.style.display = "flex";
 }
 
 function confirmOrder() {
+
+    // Getting customer infos
     const customerName = prompt("Digite seu nome");
     const customerAddress = prompt("Digite o endereço de entrega. (Rua Exemplo, nº 00, complement, bairro)");
     
+    // Calculating total
     const finalCost = myPlate.plateCost + myDrink.drinkCost + myDessert.dessertCost;
+
+    // Writing whatsapp message on right pattern
     const strMessage = "Olá, gostaria de fazer o pedido \n - Prato: " + myPlate.plateName + "\n - Bebida: " + myDrink.drinkName + "\n - Sobremesa: " + myDessert.dessertName + "\n Total: R$ " + finalCost.toFixed(2) + "\n \n Nome: " + customerName + "\n Endereço: " + customerAddress;
     const URIencodeMessage = encodeURIComponent(strMessage);
-    const wppStr = "https://wa.me/5521967670121?text=" + URIencodeMessage;
 
+    // opening whatsapp
+    const wppStr = "https://wa.me/5521967670121?text=" + URIencodeMessage;
     window.open(wppStr);
 
+    // After confirm, hide confirmWindow
     const confirmWindow = document.getElementById("confirmWindow");
     confirmWindow.style.display = "none";
 }
+
 
 function cancelOrder() {
     const confirmWindow = document.getElementById("confirmWindow");
