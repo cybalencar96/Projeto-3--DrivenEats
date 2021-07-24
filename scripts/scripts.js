bbfc9ce5631155653a90d2f6f0ca79c0a8096c40
Carlos <iuryalencar@poli.ufrj.br>
2021-07-23 21:17:14 -0300

    Refactoring - Reducing code using 'this'

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,3 @@
-// Declaring last selected controllers || -1 means that no item was selected
-let lastDessertSelected = -1;
-let lastPlateSelected = -1;
-let lastDrinkSelected = -1;
-
-
 // Creating global objects
 function plate (plateSelected,plateName,plateCost) {
     this.plateName = plateName;
@@ -11,7 +5,7 @@ function plate (plateSelected,plateName,plateCost) {
     this.plateSelected = plateSelected;
 }
 
-let myPlate = new plate;
+const myPlate = new plate;
 
 function drink (drinkSelected,drinkName,drinkCost) {
     this.drinkName = drinkName;
@@ -20,7 +14,7 @@ function drink (drinkSelected,drinkName,drinkCost) {
 
 }
 
-let myDrink = new drink;
+const myDrink = new drink;
 
 function dessert (dessertSelected,dessertName,dessertCost) {
     this.dessertName = dessertName;
@@ -28,122 +22,76 @@ function dessert (dessertSelected,dessertName,dessertCost) {
     this.plateSelected = dessertSelected;
 }
 
-let myDessert = new dessert;
+const myDessert = new dessert;
 // ----------------------------------------
 
+function selectItem(selectedItem,itemType) {
 
+    const lastItemSelected = document.querySelector("." + itemType + " .choosen");
 
-function selectItem(itemId,itemName, itemCost) {
-    const plates = document.getElementsByClassName("plates");
-    const drinks = document.getElementsByClassName("drinks");
-    const desserts = document.getElementsByClassName("desserts");
+    // if exists any selected item
+    if (lastItemSelected !== null){
+        // unmark last item selected
+        lastItemSelected.classList.remove("choosen");
+        lastItemSelected.children[4].style.display = "none";
+    }
 
-    // If item selected is a plate
-    if (itemId[0] === "0") {
+    // if new selection is the same as the last one
+    if (selectedItem === lastItemSelected) {
+        selectedItem.classList.remove("choosen");
+        selectedItem.children[4].style.display = "none";
 
-        if (Number(itemId[1]) === lastPlateSelected) {
-            plates[lastPlateSelected].classList.remove("choosen");
-            plates[lastPlateSelected].children[4].style.display = "none";
-            lastPlateSelected = -1;
+        if (itemType === 'plates') {
             myPlate.plateSelected = false;
-            enableSendButton();
-            return;
+        } else if (itemType === 'drinks') {
+            myDrink.drinkSelected = false;
+        } else if (itemType === 'desserts') {
+            myDessert.dessertSelected = false;
         }
-        // unmark last plate selected -> -1 means that no drink was selected
-        if (lastPlateSelected !== -1 ) {
-            plates[lastPlateSelected].classList.remove("choosen");
-            plates[lastPlateSelected].children[4].style.display = "none";
-        }
+        enableSendButton();
 
-        // mark new plate
-        plates[Number(itemId[1])].classList.add("choosen");
-        plates[Number(itemId[1])].children[4].style.display = "initial";
+        // exit function, no need to select again
+        return;
+    }
 
-        // update last plate selected
-        lastPlateSelected = Number(itemId[1]);
+    // select new item
+    selectedItem.classList.add("choosen");
+    selectedItem.children[4].style.display = "initial";
 
-        // Save plate name and cost
+    // Save item name and cost, workink on cost string
+    const itemName = selectedItem.children[1].innerHTML;
+    let itemCost = selectedItem.children[3].innerHTML;
+    itemCost = itemCost.replace("R$ " , "");
+    itemCost = Number(itemCost.replace("," , "."));
+
+
+    if (itemType === 'plates') {
         myPlate.plateName = itemName;
         myPlate.plateCost = itemCost;
         myPlate.plateSelected = true;
-
-    }
-    
-    // If item selected is a drink
-    if (itemId[0] === "1") {
-
-        if (Number(itemId[1]) === lastDrinkSelected) {
-            drinks[lastDrinkSelected].classList.remove("choosen");
-            drinks[lastDrinkSelected].children[4].style.display = "none";
-            lastDrinkSelected = -1;
-            myDrink.drinkSelected = false;
-            enableSendButton();
-            return;
-        }
-
-        // unmark last drink selected || -1 means that no drink was selected
-        if (lastDrinkSelected !== -1 ) {
-            drinks[lastDrinkSelected].classList.remove("choosen");
-            drinks[lastDrinkSelected].children[4].style.display = "none";
-        }
-
-        // mark new drink
-        drinks[Number(itemId[1])].classList.add("choosen");
-        drinks[Number(itemId[1])].children[4].style.display = "initial";
-
-        // update last drink selected
-        lastDrinkSelected = Number(itemId[1]);
-
-        // Save Drink name and cost
+    } else if (itemType === 'drinks') {
         myDrink.drinkName = itemName;
         myDrink.drinkCost = itemCost;
         myDrink.drinkSelected = true;
-
-    }
-    
-    // If item selected is a dessert
-    if (itemId[0] === "2") {
-
-        if (Number(itemId[1]) === lastDessertSelected) {
-            desserts[lastDessertSelected].classList.remove("choosen");
-            desserts[lastDessertSelected].children[4].style.display = "none";
-            lastDessertSelected = -1;
-            myDessert.dessertSelected = false;
-            enableSendButton();
-            return;
-        }
-
-        // unmark last item selected || -1 means that no drink was selected
-        if (lastDessertSelected !== -1 ) {
-            desserts[lastDessertSelected].classList.remove("choosen");
-            desserts[lastDessertSelected].children[4].style.display = "none";
-        }   
-
-        // mark new drink
-        desserts[Number(itemId[1])].classList.add("choosen");
-        desserts[Number(itemId[1])].children[4].style.display = "initial";
-
-        // update last drink selected
-        lastDessertSelected = Number(itemId[1]);
-
-        // Save dessert name and cost
+    } else if (itemType === 'desserts') {
         myDessert.dessertName = itemName;
         myDessert.dessertCost = itemCost;
         myDessert.dessertSelected = true;
+
     }
-    
 
     enableSendButton();
 }
+
 
 function enableSendButton() {
     const sendButton = document.getElementById("sendButton");
 
     if (myPlate.plateSelected && myDrink.drinkSelected && myDessert.dessertSelected) {
         sendButton.style.backgroundColor = "#32B72F";
-        sendButton.innerHTML = "Fechar pedido"
+        sendButton.innerHTML = "Fechar pedido";
         
-        sendButton.setAttribute("onclick", "openConfirmWindow()")
+        sendButton.setAttribute("onclick", "openConfirmWindow()");
     } else {
         sendButton.style.backgroundColor = "#CBCBCB";
         sendButton.innerHTML = "Selecione os 3 itens para fechar o pedido";
@@ -156,12 +104,13 @@ function enableSendButton() {
 
 
 function openConfirmWindow() {
+
     const orderItens = document.getElementsByClassName("confirm-order-item");
 
     // Writing plate infos on confirm window
     orderItens[0].children[0].innerHTML = myPlate.plateName;
     orderItens[0].children[1].innerHTML = myPlate.plateCost.toFixed(2);
-
+    
     // Writing drink infos on confirm window
     orderItens[1].children[0].innerHTML = myDrink.drinkName;
     orderItens[1].children[1].innerHTML = myDrink.drinkCost.toFixed(2);
@@ -176,7 +125,9 @@ function openConfirmWindow() {
 
     // Show confirm window
     const confirmWindow = document.getElementById("confirmWindow");
+        // first remove display none
         confirmWindow.classList.remove('hidden');
+        // then insert opacity 1. Using setTimeout to make JS wait until class 'hidden' is totally removed
         setTimeout(() => {
             confirmWindow.classList.remove('visuallyHidden');
         },20);
@@ -210,10 +161,16 @@ function confirmOrder() {
 
 function cancelOrder() {
     const confirmWindow = document.getElementById("confirmWindow");
+
+    // fist visually hide window with opacity
     confirmWindow.classList.add('visuallyHidden');
+
+    // when opacity transition is over, add class hidden to insert diplay none
     confirmWindow.addEventListener('transitionend', () => {
         confirmWindow.classList.add('hidden');
     }, {
+        /* making once true to listen transitionend just one time. Otherwise the function would end but the 
+         eventListener would still work */
         once: true
     });
 
